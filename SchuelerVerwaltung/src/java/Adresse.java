16d768c326cada021b57b169170443e98519cd47
medina.jose <medina.jose@PC230118.osz.intern>
2017-02-08 12:12:11 +0100

    Hinzufügung von get und set Methoden in den Klassen.

--- SchuelerVerwaltung/src/java/Adresse.java
+++ SchuelerVerwaltung/src/java/Adresse.java
@@ -1,5 +1,43 @@
 package java;
 
-public class Adresse {
+public class Adresse
+{
+private String strasse;
+private int hausNummer;
+private String wohnort;
+private int plz;
+
+public String getStrasse()
+{
+	return strasse;
+}
+public void setStrasse(String strasse)
+{
+	this.strasse = strasse;
+}
+public int getHausNummer()
+{
+	return hausNummer;
+}
+public void setHausNummer(int hausNummer)
+{
+	this.hausNummer = hausNummer;
+}
+public String getWohnort()
+{
+	return wohnort;
+}
+public void setWohnort(String wohnort)
+{
+	this.wohnort = wohnort;
+}
+public int getPlz()
+{
+	return plz;
+}
+public void setPlz(int plz)
+{
+	this.plz = plz;
+}
 
 }
