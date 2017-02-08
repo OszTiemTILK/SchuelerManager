c3f6c4abeae53df98686491dc8c27ba0d813753b
chua.bryan <chua.bryan@PC230119.osz.intern>
2017-02-08 13:17:32 +0100

    fehler ErziehungsberechtigteR

--- SchuelerVerwaltung/src/java/ErziehungsberechtigteR.java
+++ SchuelerVerwaltung/src/java/ErziehungsberechtigteR.java
@@ -1,5 +1,52 @@
 package java;
 
-public class ErziehungsberechtigteR {
+public class ErziehungsberechtigteR extends Adresse
+{
+	private int typ;
+	private String vorname;
+	private String nachname;
+	private boolean vormund;
 
+	public int getTyp()
+	{
+		return typ;
+	}
+
+	public void setTyp(int ptyp)
+	{
+		this.typ = ptyp;
+	}
+
+	public String getVorname()
+	{
+		return vorname;
+	}
+
+	public void setVorname(String pvorname)
+	{
+		this.vorname = pvorname;
+	}
+
+	public String getNachname()
+	{
+		return nachname;
+	}
+
+	public void setNachname(String pnachname)
+	{
+		this.nachname = pnachname;
+	}
+
+	public boolean isVormund()
+	{
+		return vormund;
+	}
+
+	public void setVormund(boolean pvormund)
+	{
+		this.vormund = pvormund;
+	}
 }
+
+
+
--- SchuelerVerwaltung/src/java/Herkunft.java
+++ SchuelerVerwaltung/src/java/Herkunft.java
@@ -1,5 +1,45 @@
 package java;
 
-public class Herkunft {
+public class Herkunft
+{
+private String staatsangehörigkeit;
+private String volksgruppe;
+private String geburtsort;
+private String geburtsland;
+
+public String getStaatsangehörigkeit()
+{
+
+	return staatsangehörigkeit;
+}
+public void setStaatsangehörigkeit(String pStaatsangehörigkeit)
+{
+
+	this.staatsangehörigkeit = pStaatsangehörigkeit;
+}
+public String getVolksgruppe()
+{
+	return volksgruppe;
+}
+public void setVolksgruppe(String pVolksgruppe)
+{
+	this.volksgruppe = pVolksgruppe;
+}
+public String getGeburtsort()
+{
+	return geburtsort;
+}
+public void setGeburtsort(String pGeburtsort)
+{
+	this.geburtsort = pGeburtsort;
+}
+public String getGeburtsland()
+{
+	return geburtsland;
+}
+public void setGeburtsland(String pGeburtsland)
+{
+	this.geburtsland = pGeburtsland;
+}
 
 }
