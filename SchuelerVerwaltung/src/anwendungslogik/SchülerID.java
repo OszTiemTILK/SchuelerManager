6a83b5bd3c8375cf65bb819431966b0da28efdda
wildt.rico <wildt.rico@PC230121.osz.intern>
2017-02-08 14:54:54 +0100

    Klasse SchülerID erstellt und Methoden erstellt von Aslan

---
+++ SchuelerVerwaltung/src/anwendungslogik/SchülerID.java
@@ -0,0 +1,35 @@
+package anwendungslogik;
+
+public class SchülerID {
+
+	private int schülerID;
+
+	/** legt schülerID an
+	 *
+	 */
+	public void anlegenSchuelerID()
+	{
+
+	}
+	/** aendert SchülerID
+	 *
+	 */
+	public void aendernSchuelerID()
+	{
+
+	}
+	/** loescht SchuelerID
+	 *
+	 */
+	public void loeschenSchuelerID()
+	{
+
+	}
+	/** speichert SchuelerID
+	 *
+	 */
+	public void speichernSchuelerID()
+	{
+
+	}
+}
