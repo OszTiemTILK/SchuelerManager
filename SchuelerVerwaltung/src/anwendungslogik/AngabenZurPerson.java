be2c75354612af9f162cd0eab26718b361ffb0c6
Hohenstein.Fabian <hohenstein.fabian@web.de>
2017-02-15 13:59:17 +0100

    Bis auf "AngabenZurPerson" und Kontaktdaten sollte die Klasse funktionieren. Änderungen in SchülerID, Implimentierung von "ausgabe" in verschiedenen Klassen

--- SchuelerVerwaltung/src/anwendungslogik/AngabenZurPerson.java
+++ SchuelerVerwaltung/src/anwendungslogik/AngabenZurPerson.java
@@ -30,6 +30,105 @@ public void loeschenAngabenZurPerson()
 {
 
 }
+/** Simuliert speicherung der Angaben zur Person
+ *
+ */
+public void ausgeben()
+{
+	System.out.println("Name: " + vorname + " " + nachname);
+	System.out.println("Geburtsdatum: " + getGeburtsDatum() );
+	System.out.print("Geschlecht: " + geschlecht);
+}
+
+
+
+
+
+
+
+
+
+public String getVorname()
+{
+	return vorname;
+}
+
+
+public void setVorname(String pVorname)
+{
+	this.vorname = pVorname;
+}
+
+
+public String getNachname()
+{
+	return nachname;
+}
+
+
+public void setNachname(String pNachname)
+{
+	this.nachname = pNachname;
+}
+
+
+public LocalDate getGeburtsDatum()
+{
+	return geburtsDatum;
+}
+
+
+public void setGeburtsDatum(LocalDate pGeburtsDatum)
+{
+	this.geburtsDatum = pGeburtsDatum;
+}
+
+
+public Geschlecht getGeschlecht()
+{
+	return geschlecht;
+}
+
+
+public void setGeschlecht(Geschlecht pGeschlecht)
+{
+	this.geschlecht = pGeschlecht;
+}
+
+
+}
+package anwendungslogik;
+
+import java.time.LocalDate;
+
+public class AngabenZurPerson
+{
+private String vorname;
+private String nachname;
+private LocalDate geburtsDatum;
+private Geschlecht geschlecht;
+
+/** legt Angaben zur Person an
+ *
+ */
+public void anlegenAngabenZurPerson()
+{
+
+}
+/** aendert Angaben zur Person
+ *
+ */
+public void aendernAngabenZurPerson()
+{
+
+}
+/** loescht Angaben zur Person
+ *
+ */
+public void loeschenAngabenZurPerson()
+{
+
+}
 /** speichert Angaben zur Person
  *
  */
@@ -39,6 +138,7 @@ public void ausgebenKonsoleTest()
 	System.out.println("Name: " + vorname + " " + nachname);
 	System.out.println("Geburtsdatum: " + getGeburtsDatum() );
 	System.out.println("Geschlecht: " + geschlecht);
+}
 
 public void speichernAngabenZurPerson()
 {
