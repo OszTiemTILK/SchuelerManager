706d11cdc556f66a9819ede8609395a02423217a
albrecht.oliver <albrecht.oliver@PC230109.osz.intern>
2017-03-01 13:19:47 +0100

    In der Klasse Adresse speichernAdresse erstellt

--- SchuelerVerwaltung/src/anwendungslogik/Adresse.java
+++ SchuelerVerwaltung/src/anwendungslogik/Adresse.java
@@ -1,4 +1,4 @@
-package anwendungslogik;
+﻿package anwendungslogik;
 
 public class Adresse
 {
@@ -7,8 +7,25 @@ private int hausNummer;
 private String wohnort;
 private int plz;
 
-//Nur set- und get-Methoden ab hier 
-  
+public Adresse(String pStrasse, int pHausnummer, String pWohnort, int pPlz)
+{
+	this.strasse = pStrasse;
+	this.hausNummer = pHausnummer;
+	this.wohnort = pWohnort;
+	this.plz = pPlz;
+}
+
+//Nur set- und get-Methoden ab hier  ... und ausgeben
+
+public void anlegenAdresse()
+{
+
+}
+
+/**
+ * Nur set- und get-Methoden ab hier
+ */
+
 public String getStrasse()
 {
 	return strasse;
@@ -40,6 +57,16 @@ public int getPlz()
 public void setPlz(int pPlz)
 {
 	this.plz = pPlz;
+}
+
+public void ausgeben()
+{
+	System.out.print("Straße: " + getStrasse());
+	System.out.print("Nr. " + getHausNummer());
+	System.out.println();
+	System.out.print("Wohnort: " + getWohnort());
+	System.out.print("PLZ: " + getPlz());
+	System.out.println();
 }
 
 }
