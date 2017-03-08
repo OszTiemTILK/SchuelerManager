a469714167bd70b99a25f1b5bdc8af57190bde3a
Katzen48 <Katzen48@freenet.de>
2017-03-08 14:25:01 +0100

    LehrmittelController angelegt

---
+++ SchuelerVerwaltung/src/fx/LehrmittelController.java
@@ -0,0 +1,106 @@
+package fx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+
+public class LehrmittelController
+{
+    @FXML
+    private ChoiceBox<?> cbArt4;
+
+    @FXML
+    private ChoiceBox<?> cbArt5;
+
+    @FXML
+    private ChoiceBox<?> cbName2;
+
+    @FXML
+    private ChoiceBox<?> cbName3;
+
+    @FXML
+    private ChoiceBox<?> cbName4;
+
+    @FXML
+    private ChoiceBox<?> cbName5;
+
+    @FXML
+    private ChoiceBox<?> cbArt1;
+
+    @FXML
+    private ChoiceBox<?> cbArt2;
+
+    @FXML
+    private ChoiceBox<?> cbArt3;
+
+    @FXML
+    private ChoiceBox<?> cbName1;
+
+    @FXML
+    private DatePicker dpAusgegeben1;
+
+    @FXML
+    private DatePicker dpAusgegeben3;
+
+    @FXML
+    private DatePicker dpAusgegeben2;
+
+    @FXML
+    private Button btSpeichern;
+
+    @FXML
+    private ChoiceBox<?> chFach3;
+
+    @FXML
+    private Button btZuruecksetzen;
+
+    @FXML
+    private DatePicker dpAusgegeben5;
+
+    @FXML
+    private DatePicker dpAusgegeben4;
+
+    @FXML
+    private TextArea taName;
+
+    @FXML
+    private ChoiceBox<?> cbFach2;
+
+    @FXML
+    private ChoiceBox<?> cbFach1;
+
+    @FXML
+    private CheckBox cbRückgabe1;
+
+    @FXML
+    private CheckBox cbRückgabe4;
+
+    @FXML
+    private ChoiceBox<?> cbFach5;
+
+    @FXML
+    private CheckBox cbRückgabe5;
+
+    @FXML
+    private ChoiceBox<?> cbFach4;
+
+    @FXML
+    private CheckBox cbRückgabe2;
+
+    @FXML
+    private CheckBox cbRückgabe3;
+
+
+    public void drückenSpeichern()
+    {
+
+    }
+
+    public void drückenZuruecksetzen()
+    {
+
+    }
+}
