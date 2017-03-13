164c14d4f5865beed6ea4e562ad56c51ae349bb3
suekuer.onur <suekuer.onur@PC230112.osz.intern>
2017-03-13 10:27:16 +0100

    Maske bearbeitet und methoden eingefügt

---
+++ SchuelerVerwaltung/src/fx/SprachenController.java
@@ -0,0 +1,53 @@
+package fx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control. ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control. TextArea;
+import javafx.scene.control. CheckBox;
+
+
+public class SprachenController {
+
+	    @FXML
+	    private TextField tfTitel;
+
+	    @FXML
+	    private TextField tfSprachen;
+
+	    @FXML
+	    private TextArea taText ;
+
+	    @FXML
+	    private CheckBox ckDeutsch;
+
+	    @FXML
+	    private CheckBox ckEnglisch;
+
+	    @FXML
+	    private CheckBox ckTürkisch;
+
+	    @FXML
+	    private CheckBox cbArabisch;
+
+	    @FXML
+	    private CheckBox ckSonstiges;
+
+	    @FXML
+	    private ChoiceBox cbDeutschNiveau;
+
+	    @FXML
+	    private ChoiceBox cbEnglischNiveau;
+
+	    @FXML
+	    private ChoiceBox cbTürkischNiveau;
+
+	    @FXML
+	    private ChoiceBox cbArabischNiveau;
+
+	    @FXML
+	    private ChoiceBox cbSonstigesNiveau;
+
+}
+
