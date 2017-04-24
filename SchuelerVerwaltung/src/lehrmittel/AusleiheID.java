2caca4856a23f61f30e2b1fff394dfb584ff8db2
kainz.felix_valentin <kainz.felix@web.de>
2017-04-24 10:49:21 +0200

    Datenbankzugriff für db_schulprojekt.ausgeliehen auf IDAusleihe erstellt

--- SchuelerVerwaltung/src/lehrmittel/AusleiheID.java
+++ SchuelerVerwaltung/src/lehrmittel/AusleiheID.java
@@ -1,20 +1,17 @@
 package lehrmittel;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import datenbank.Datenbankverbindung;
+
 public class AusleiheID
 {
-	int vorherigeID;
 	int ausleiheID;
 	public AusleiheID()
 	{
-		ausleiheID = vorherigeID + 1;
-	}
-	public int getVorherigeID()
-	{
-		return vorherigeID;
-	}
-	public void setVorherigeID(int pvorherigeID)
-	{
-		this.vorherigeID = pvorherigeID;
+		ausleiheID = holenNächsteAusleiheID();
 	}
 	public int getAusleiheID()
 	{
@@ -23,6 +20,29 @@ public class AusleiheID
 	public void setAusleiheID(int pausleiheID)
 	{
 		this.ausleiheID = pausleiheID;
+	}
+
+	public int holenNächsteAusleiheID()
+	{
+		int nächsteAusleiheID = 0;
+
+		Statement stmt = null;
+		ResultSet result = null;
+		Connection lConnection = Datenbankverbindung.holen();
+		try
+		{
+			stmt = lConnection.createStatement();
+			result = stmt.executeQuery("Select IDAusleihe FROM db_schulprojekt.ausgeliehen a ORDER BY IDAusleihe desc ");
+			result.first();
+			nächsteAusleiheID = result.getInt("IDAusleihe") +1;
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Fehler bei der Verarbeitung + " + "Lernmittel" + " " + ex.getMessage());
+		}
+		System.out.println(nächsteAusleiheID);
+		return nächsteAusleiheID;
+
 	}
 
 }
