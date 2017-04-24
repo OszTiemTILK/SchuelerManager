4dd373ea2df633e6c67792d8fe94d196718c8230
kaufmann.aslan_alexa <kaufmann.aslan_alexa@PC230121.osz.intern>
2017-04-24 10:21:55 +0200

    Aslan, höchste ID Methode hinzugefügt

--- SchuelerVerwaltung/src/Schullaufbahn/IDSchule.java
+++ SchuelerVerwaltung/src/Schullaufbahn/IDSchule.java
@@ -2,20 +2,24 @@
 package Schullaufbahn;
 
 import java.lang.reflect.Array;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.Normalizer.Form;
 import java.util.ArrayList;
 
 public class IDSchule
 {
     private static int aktuelleIDSchule = getAktuelleIDSchule(); //letzte vergebene SchülerID
 
-	private static int IDSchule = 4;
+	private static int IDSchule = 0;
 
 //  Konstruktoren
 
 	IDSchule()
 	{
-	     IDSchule = aktuelleIDSchule + 1;
-		setAktuelleIDSchule(IDSchule);
+		getHöchsteID(IDSchule);
+		IDSchule ++;
 	}
 
 	IDSchule(int pIDSchuleFest)
@@ -42,5 +46,35 @@ public class IDSchule
     {
     	aktuelleIDSchule = paktuelleIDSchule;
 	}
+
+    public static IDSchule getHöchsteID(int IDSchule)
+    {
+    	Connection lConnection = DBVerbindungSchullaufbahn.holen();
+    	IDSchule lIDSchule;
+    	ArrayList<IDSchule> lIDSchuleListe = new ArrayList<IDSchule>();
+    	Statement lBefehl;
+    	ResultSet lErgebnis;
+
+    	try {
+    		lBefehl = lConnection.createStatement();
+    		lErgebnis= lBefehl.executeQuery("SELECT MAX(IDSchule) FROM db_schulprojekt.besucht");
+    		lErgebnis.first();
+
+    		while(! lErgebnis.isAfterLast())
+    		{
+    			lIDSchule = new IDSchule (lErgebnis.getInt(4));
+    			lIDSchuleListe.add(lIDSchule);
+
+    			return lIDSchule;
+    		}
+
+    	 } catch (Exception ex)
+  			{
+    		 	System.out.println("Fehler bei der Verarbeitung + " + "n" + ex.getMessage());
+  			}
+    	return null;
+
+    	}
+
 
 }
