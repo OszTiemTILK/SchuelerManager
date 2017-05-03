fe984cabdbd110f864d3c68e003e069d906848a3
albrecht.oliver <albrecht.oliver@PC230107.osz.intern>
2017-05-03 13:45:42 +0200

    Fehler behoben

--- SchuelerVerwaltung/src/adresse/AdresseController.java
+++ SchuelerVerwaltung/src/adresse/AdresseController.java
@@ -14,71 +14,39 @@ public class AdresseController {
     private Button btWeiter;
 
     @FXML
-    private TextField tfWohnort3;
-
-    @FXML
-    private TextField tfWohnort2;
-
-    @FXML
     private TextField tfWohnort1;
-
-    @FXML
-    private ProgressBar pbFortschritt;
 
     @FXML
     private TextField tfPlz1;
 
     @FXML
-    private TextField tfFestnetzHandynummer2;
-
-    @FXML
-    private TextField tfPlz3;
-
-    @FXML
-    private TextField tfFestnetzHandynummer1;
-
-    @FXML
-    private TextField tfPlz2;
-
-    @FXML
-    private TextField tfFestnetzHandynummer3;
-
-    @FXML
-    private TextField tfFax1;
-
-    @FXML
-    private TextField tfEmail1;
-
-    @FXML
-    private TextField tfFax2;
-
-    @FXML
     private TextField tfAdresseStraßeMitHausnummer1;
-
-    @FXML
-    private TextField tfEmail2;
-
-    @FXML
-    private TextField tfFax3;
-
-    @FXML
-    private TextField tfEmail3;
-
-    @FXML
-    private TextField tfAdresseStraßeMitHausnummer3;
-
-    @FXML
-    private TextField tfAdresseStraßeMitHausnummer2;
 
     @FXML
     private Button btZurück;
 
     @FXML
-    void anlegenKontaktdaten(ActionEvent event) 
+    private TextField tfVorname;
+
+    @FXML
+    private TextField tfNachname;
+
+    @FXML
+    private Button btSchülerIDsuchen;
+
+    @FXML
+    void anlegenKontaktdaten(ActionEvent event)
     {
     	Adresse lAdresse = new Adresse(tfAdresseStraßeMitHausnummer1.getText(), tfWohnort1.getText(), Integer.parseInt(tfPlz1.getText()));
     	lAdresse.anlegenAdresse();
-    	
+
+    }
+
+    @FXML
+    void suchenSchülerID(ActionEvent event)
+    {
+    	AngabenZurPerson lAngabenZurPerson = new AngabenZurPerson(tfVorname.getText(), tfNachname.getText());
+    	lAngabenZurPerson.suchenSchülerInID();
     }
 
 }
