d0d771956c8352e120e8895331eeadc7d000b221
henderkes.florian <henderkes.florian@PC230101.osz.intern>
2017-05-31 13:38:07 +0200

    Adresse bearbeitet

--- SchuelerVerwaltung/src/adresse/AdresseController.java
+++ SchuelerVerwaltung/src/adresse/AdresseController.java
@@ -29,13 +29,31 @@ public class AdresseController {
     private TextField tfAdresseStraßeMitHausnummer1;
 
     @FXML
+    private TextField tfOrtSuchen;
+
+    @FXML
+    private TextField tfPlzSuchen;
+
+    @FXML
+    private TextField tfStrasseSuchen;
+
+    @FXML
+    private TextField tfHausNrSuchen;
+
+    @FXML
     private Button btZurück;
 
     @FXML
     private Button btSchülerIDsuchen;
 
     @FXML
+    private Button btAdresseIDsuchen;
+
+    @FXML
     private TextField tfSuchAusgabe;
+
+    @FXML
+    private TextField tfAusgabeAdresseID;
 
     @FXML
     void anlegenKontaktdaten(ActionEvent event)
@@ -49,16 +67,20 @@ public class AdresseController {
     void suchenSchülerID(ActionEvent event)
     {
     	AngabenZurPerson lAngabenZurPerson = new AngabenZurPerson(tfVorname.getText(), tfNachname.getText());
-    	lAngabenZurPerson.suchenSchülerInID();
-
     	int lSchülerInID;
-
     	if((lSchülerInID = lAngabenZurPerson.suchenSchülerInID()) != -1)
-    	{
     		tfSuchAusgabe.setText(String.valueOf(lSchülerInID));
-    	}
+    }
 
-    
+    @FXML
+    public void suchenAdresseID()
+    {
+    	Adresse lAdresse = new Adresse(tfStrasseSuchen.getText(), Integer.parseInt(tfHausNrSuchen.getText()), tfOrtSuchen.getText(), Integer.parseInt(tfPlzSuchen.getText()));
+    	int lAdresseID;
+    	if((lAdresseID = lAdresse.suchenAdresseID()) != -1)
+    		tfAusgabeAdresseID.setText(String.valueOf(lAdresseID));
+    	else
+    		tfAusgabeAdresseID.setText("Nicht vorhanden!");
     }
 
     @FXML
